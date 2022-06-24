a58e3bb71e386fae446061bc2e66182f9560804f
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-06-24 15:09:49 +0530

    added piano file

---
+++ piano/app.js
@@ -0,0 +1,11 @@
+const whiteKeysRef = document.querySelectorAll("a");
+var playNote = (element, idx) => {
+    let classListOfElement = element.classList;
+    let audio = new Audio('audio/key-' + (idx + 1) + '.mp3');
+    console.log(idx);
+    audio.play();
+}
+
+whiteKeysRef.forEach((element, idx) => {
+    element.addEventListener('click', () => playNote(element, idx));
+});
